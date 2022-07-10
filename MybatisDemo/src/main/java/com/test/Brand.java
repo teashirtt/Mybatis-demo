06c074787d501fd26c64f4100d26da24db78a806
Yennefer <1223696764@qq.com>
2022-07-10 23:58:11 +0800

    without mapper agent verison

---
+++ MybatisDemo/src/main/java/com/test/Brand.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import lombok.Data;
+
+@Data
+public class Brand {
+    private Integer id;
+    private String brand;
+    private String companyName;
+    private Integer ordered;
+    private String description;
+    private Integer status;
+}
